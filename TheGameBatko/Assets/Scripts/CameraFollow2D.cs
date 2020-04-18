826c5b81950fdfba72b14ed8a54dc47761b6dcbe
zerro123321 <62381954+zerro123321@users.noreply.github.com>
2020-04-18 20:23:13 +0300

    Last Commit 18.04 - 2

---
+++ TheGameBatko/Assets/Scripts/CameraFollow2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Camera start position
+        Vector3 startPos = transform.position;
+
+        //Player current position
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+    }
+}
