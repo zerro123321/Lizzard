f81e8f60cfa6e1a1542ab75ecccb38ad45eedd01
zerro123321 <62381954+zerro123321@users.noreply.github.com>
2020-04-20 01:03:52 +0300

    Last Commit 20.04  01:03

---
+++ TheGameBatko/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    bool musicOn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (PlayerPrefs.HasKey("MusicEnable"))
+        {
+            if (PlayerPrefs.GetString("MusicEnable") == "True")
+            {
+                musicOn = true;
+            }
+
+            else if (PlayerPrefs.GetString("MusicEnagle") == "False")
+            {
+                musicOn = false;
+            }
+        }
+
+        TryPlayMusic();
+    }
+    void TryPlayMusic()
+    {
+        if (musicOn)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        else
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        if (musicOn)
+        {
+            musicOn = false;
+            PlayerPrefs.SetString("MusicEnabled", "False");
+        }
+        else
+        {
+            musicOn = true;
+            PlayerPrefs.SetString("MusicEnabled", "False");
+        }
+        TryPlayMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+        
---
+++ TheGameBatko/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameScript : MonoBehaviour
+{
+    public string nextLevelSceenName = "Level 2";
+    public void StartGame()
+    {
+
+        SceneManager.LoadScene(nextLevelSceenName);
+
+    }
+}
