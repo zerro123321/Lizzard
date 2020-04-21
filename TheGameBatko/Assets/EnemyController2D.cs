6d3c4a3749dc9ef44b8827c41b9a68e08f050c29
zerro123321 <62381954+zerro123321@users.noreply.github.com>
2020-04-21 19:47:38 +0300

    Last Commit 21.04 - 19:47

---
+++ TheGameBatko/Assets/EnemyController2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyController2D : MonoBehaviour
+{
+    private AIPath aiEnemy;
+    private Animator animEnemy;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        aiEnemy = GetComponent<AIPath>();
+        animEnemy = GetComponent<Animator>();
+    }
+
+    void FixUpdate()
+    {
+        if (aiEnemy.maxSpeed >= 0.01f)
+        {
+            
+            Debug.Log("Walking");
+
+        }
+
+        if (facingRight == false && aiEnemy.maxSpeed > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && aiEnemy.maxSpeed < 0)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
